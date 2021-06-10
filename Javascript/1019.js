6548bfce2b2da2b43b2586fd6a155e8009256faa
Vyctor <dev.vyctor@gmail.com>
2021-06-10 00:18:34 -0300

    feat: add problem enunciates

--- Javascript/1019.js
+++ Javascript/1019.js
@@ -1,3 +1,14 @@
+/**
+ * Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica,
+ * e informe-o expresso no formato horas:minutos:segundos.
+ *
+ * Entrada
+ * O arquivo de entrada contém um valor inteiro N.
+ *
+ * Saída
+ * Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+ */
+
 var input = require("fs").readFileSync("/dev/stdin", "utf8");
 var lines = input.split("\n");
 
